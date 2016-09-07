975cb2e925926a58ab1cbd126befe65e0e62e642
cathylu10 <LuC3424@sduhsd.lan>
2016-09-07 14:03:57 -0700

    Added files

---
+++ Lab_02.java
@@ -0,0 +1,25 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 5;
+		int b = 17;
+		int answer = a * b;
+		
+		System.out.println(a + " multiplied by " + b + " is " + answer);
+		
+		String name = "Cathy Lu";
+		String address = "3710 Del Mar Heights Rd";
+		String city = "San Diego";
+		String zip = "92130";
+		
+		System.out.println(name + "\n" + address + "\n" + city + "\n" + zip);
+		
+		int l = 3;
+		int w = 4;
+		int h = 5;
+		int SA = l * w * h;
+		
+		System.out.println("The surface area of your rectangel is " + SA);
+	}
+}
